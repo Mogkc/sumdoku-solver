b1074dbe27160cbd008ec3ea4604505b510207ea
Mogkc <carson.mogk@cox.net>
2019-09-30 08:54:59 -0400

    SumdokuBoard can get(row, col)

--- main.js
+++ main.js
@@ -7,7 +7,34 @@ class SumdokuBoard {
         if(!Number.isInteger(this.size))
             throw new InvalidInputError("Not a square board");
     }
+    get(row, col) {
+        return this.board[(this.size * row) + col];
+    }
+    /* On hold until get(row, col) created
+    row(row) {
+        return {
+            [Symbol.iterator]() {
+                return new RowIterator(this, row);
+            }
+        }
+    }
+    */
 }
+
+/*
+class RowIterator {
+    constructor(SumdokuBoard, row) {
+        this.row = row;
+        this.index = 0;
+        this.board = SumdokuBoard;
+    }
+    next() {
+        if(this.index == this.board.size)
+            return { done: true }
+        let value = this.board.get(row, index); // board.get not created yet!
+    }
+}
+*/
 
 module.exports['SumdokuBoard'] = SumdokuBoard;
 module.exports['InvalidInputError'] = InvalidInputError;
--- test/main.js
+++ test/main.js
@@ -9,22 +9,35 @@ describe("test script", () => {
 });
 
 describe("SumdokuBoard", () => {
-    const input = [1,2,3,4,5,6,7,8,9];
-    it("should accept an array", () => {
-        assert.equal(input, new SumdokuBoard(input).board);
+    const input = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    describe("constructor", () => {
+        it("should accept an array", () => {
+            assert.equal(input, new SumdokuBoard(input).board);
+        });
+        it("should have a size equal to the square root of its array", () => {
+            assert.equal(Math.sqrt(input.length), new SumdokuBoard(input).size);
+        });
+        it("should reject non-square boards", () => {
+            try {
+                new SumdokuBoard(new Array(2));
+                assert.isTrue(false);
+            } catch (e) {
+                if (e instanceof InvalidInputError)
+                    assert.isTrue(true);
+                else throw e;
+            }
+        });
     });
-    it("should have a size equal to the square root of its array", () => {
-        assert.equal(Math.sqrt(input.length), new SumdokuBoard(input).size);
-    });
-    it("should reject non-square boards", () => {
-        try {
-            new SumdokuBoard(new Array(2));
-            assert.isTrue(false);
-        } catch(e) {
-            console.log(typeof e);
-            if(e instanceof InvalidInputError)
-                assert.isTrue(true);
-            else throw e;
-        }
+    describe("get", () => {
+        it("should get contents by row and column", () => {
+            const board = new SumdokuBoard(input)
+            assert.equal(1, board.get(0, 0));
+            assert.equal(9, board.get(2, 2));
+        });
+        /* Put on hold as it builds upon other things not yet built
+        it("should be iterable by row", () => {
+            const iterator = new SumdokuBoard(input).row(1)[Symbol.iterator]();
+        });
+        */
     });
 });
