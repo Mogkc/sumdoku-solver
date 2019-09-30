8ec11fc6a13a2741733ec557593c83a70a023138
Mogkc <carson.mogk@cox.net>
2019-09-30 08:23:27 -0400

    SumdokuBoard rejects non-square inputs

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
+class InvalidInputError extends Error {}
+
 class SumdokuBoard {
     constructor(array) {
         this.board = array;
         this.size = Math.sqrt(this.board.length);
+        if(!Number.isInteger(this.size))
+            throw new InvalidInputError("Not a square board");
     }
 }
 
 module.exports['SumdokuBoard'] = SumdokuBoard;
+module.exports['InvalidInputError'] = InvalidInputError;
--- test/main.js
+++ test/main.js
@@ -1,5 +1,6 @@
 const assert = require('chai').assert;
 const SumdokuBoard = require('../main').SumdokuBoard;
+const InvalidInputError = require('../main').InvalidInputError;
 
 describe("test script", () => {
     it("should run", () => {
@@ -14,5 +15,16 @@ describe("SumdokuBoard", () => {
     });
     it("should have a size equal to the square root of its array", () => {
         assert.equal(Math.sqrt(input.length), new SumdokuBoard(input).size);
+    });
+    it("should reject non-square boards", () => {
+        try {
+            new SumdokuBoard(new Array(2));
+            assert.isTrue(false);
+        } catch(e) {
+            console.log(typeof e);
+            if(e instanceof InvalidInputError)
+                assert.isTrue(true);
+            else throw e;
+        }
     });
 });
