1678ffddc10dfa22c1c2b251461de230ca605114
Mogkc <carson.mogk@cox.net>
2019-09-30 08:00:28 -0400

    SumdokuBoard accepts an array

--- test/main.js
+++ test/main.js
@@ -1,7 +1,15 @@
 const assert = require('chai').assert;
+const SumdokuBoard = require('../main');
 
 describe("test script", () => {
     it("should run", () => {
         assert.isTrue(true);
+    });
+});
+
+describe("SumdokuBoard", () => {
+    it("Should accept an array", () => {
+        const input = [1,2,3,4,5,6,7,8,9]
+        assert.equal(input, new SumdokuBoard(input).board);
     });
 });
