6dacaf530d8242401b979a51cdcae9efdbf9bb2a
Mogkc <carson.mogk@gmail.com>
2019-10-04 15:55:52 -0400

    started working on solver

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
 const SumdokuBoard = require('./models/SumdokuBoard');
 const Group = require('./models/Group')
+
+module.exports = function (boardState) {
+    return boardState;
+}
--- test/main.js
+++ test/main.js
@@ -4,3 +4,12 @@ const testGroup = require("./testGroup");
 
 testSumdokuBoard(assert);
 testGroup(assert);
+
+const solver = require("../main");
+const SumdokuBoard = require("../models/SumdokuBoard")
+
+describe("solver", () => {
+    it("should take and return SumdokuBoards", () => {
+        assert.isTrue(solver(SumdokuBoard.newBoard()) instanceof SumdokuBoard);
+    });
+});
