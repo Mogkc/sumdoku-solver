60878e09dcb486b22526388b44806b33b7168dee
Mogkc <carson.mogk@gmail.com>
2019-10-04 15:48:07 -0400

    refactored SumdokuBoard constructor

--- models/SumdokuBoard.js
+++ models/SumdokuBoard.js
@@ -7,18 +7,22 @@ class SumdokuBoard {
         this.size = Math.sqrt(this.board.length);
         if (!Number.isInteger(this.size))
             throw new InvalidInputError("Not a square board");
-        // Set up the basic groups all sudoku games share
-        this.groups = groups || [];
-        this.possibilities = [];
-        for(let register = 0; register < this.size; register++) {
-            this.possibilities.push(register);
-            const row = [], column = [];
-            for(let  range = 0; range < this.size; range++) {
-                row.push([register, range]);
-                column.push([range, register]);
+        if (groups) {
+            this.groups = groups;
+        } else {
+            // Set up the basic groups all sudoku games share
+            this.groups = [];
+            this.possibilities = [];
+            for (let register = 0; register < this.size; register++) {
+                this.possibilities.push(register);
+                const row = [], column = [];
+                for (let range = 0; range < this.size; range++) {
+                    row.push([register, range]);
+                    column.push([range, register]);
+                }
+                this.groups.push(new Group(row, 45, this.possibilities, undefined, true));
+                this.groups.push(new Group(column, 45, this.possibilities, undefined, true));
             }
-            this.groups.push(new Group(row, 45, this.possibilities, undefined, true));
-            this.groups.push(new Group(column, 45, this.possibilities, undefined, true));
         }
     }
     _notInSize(val) {
@@ -59,14 +63,14 @@ SumdokuBoard.newBoard = function () {
     const blankBoard = new SumdokuBoard(allPossible);
     // Add the 3x3 box groupings
     const locations = new Array(9);
-    
+
     // By moving the upperleft corner
-    for(let x = 0; x < 9; x += 3) {
-        for(let y = 0; y < 9; y += 3) {
+    for (let x = 0; x < 9; x += 3) {
+        for (let y = 0; y < 9; y += 3) {
             // and filling in below and right
-            for(let row = 0; row < 3; row++) {
-                for(let col = 0; col < 3; col++) {
-                    locations[row*3 + col] = [x+row, y+col];
+            for (let row = 0; row < 3; row++) {
+                for (let col = 0; col < 3; col++) {
+                    locations[row * 3 + col] = [x + row, y + col];
                 }
             }
             blankBoard.groups.push(new Group(locations, undefined, possibilities, undefined, true));
